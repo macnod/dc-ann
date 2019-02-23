16755182d9b0a998f9a0940160c9eb36f6b40090
Donnie Cameron <macnod@gmail.com>
2019-02-23 07:37:30 -0800

    Tweaks

--- package.lisp
+++ package.lisp
@@ -1,7 +1,26 @@
 ;;;; package.lisp
 
 (defpackage :dc-ann
-  (:use :cl :dc-utilities :sb-thread :cl-ppcre)
-  (:export t-cx t-neuron t-layer t-net randomize-weights feed train
-           ann-freeze ann-thaw run-test make-net anneal tset-to-file
-           id log-file stop-training evaluate-one-hotshot-training))
+  (:use :cl 
+        :cl-ppcre
+        :dc-utilities
+        :hunchentoot
+        :ht-routes
+        :sb-thread)
+  (:export ann-freeze
+           ann-thaw
+           anneal
+           evaluate-one-hotshot-training
+           feed
+           id
+           log-file
+           make-net
+           randomize-weights
+           run-test
+           stop-training
+           t-cx
+           t-layer
+           t-net
+           t-neuron
+           train
+           tset-to-file))
